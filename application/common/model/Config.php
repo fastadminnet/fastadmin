bcdf824368dda90353b34b223e466161af3c6180
Karson <karsonzhang@163.com>
2025-04-30 17:28:30 +0800

    新增Config配置超时配置

--- application/common/model/Config.php
+++ application/common/model/Config.php
@@ -189,6 +189,7 @@ class Config extends Model
             'savekey'    => $uploadcfg['savekey'],
             'multipart'  => [],
             'multiple'   => $uploadcfg['multiple'],
+            'timeout'    => $uploadcfg['timeout'],
             'fullmode'   => $uploadcfg['fullmode'],
             'thumbstyle' => $uploadcfg['thumbstyle'],
             'storage'    => 'local'
