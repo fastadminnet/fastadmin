016b7a0d28ee095fd90a6ebcfe132e8902ce40e7
Karson <karsonzhang@163.com>
2025-06-26 16:54:34 +0800

    优化登录后跳转

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -70,7 +70,7 @@ class Index extends Backend
     public function login()
     {
         $url = $this->request->get('url', '', 'url_clean');
-        $url = $url ?: 'index/index';
+        $url = $url && !preg_match('/\/index\/(login|logout)$/', $url) ? $url : 'index/index';
         if ($this->auth->isLogin()) {
             $this->success(__("You've logged in, do not login again"), $url);
         }
