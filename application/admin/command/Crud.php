5f70f0ceae939a1f854aa56ceb3ff06a3dd077ef
sixXing <464401240@qq.com>
2025-09-25 03:55:40 +0000

    update application/admin/command/Crud.php.
    Signed-off-by: sixXing <464401240@qq.com>

--- application/admin/command/Crud.php
+++ application/admin/command/Crud.php
@@ -1553,6 +1553,7 @@ EOD;
                 $valArr = explode('=', $v);
                 if (count($valArr) == 2) {
                     list($key, $value) = $valArr;
+                    $key = trim($key);
                     $itemArr[$key] = $field . ' ' . $key;
                 }
             }
