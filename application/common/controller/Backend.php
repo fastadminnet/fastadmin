b430f058a9fb9b10cb336d84bcbffe56c7eda34a
Karson <karsonzhang@163.com>
2026-05-20 17:36:21 +0800

    修复Selectpage自定义筛选安全问题
    # Conflicts:
    #	application/common/controller/Backend.php

--- application/common/controller/Backend.php
+++ application/common/controller/Backend.php
@@ -3,6 +3,7 @@
 namespace app\common\controller;
 
 use app\admin\library\Auth;
+use app\common\library\SelectPage;
 use think\Config;
 use think\Controller;
 use think\Hook;
@@ -10,7 +11,6 @@ use think\Lang;
 use think\Loader;
 use think\Model;
 use think\Session;
-use fast\Tree;
 use think\Validate;
 
 /**
@@ -468,144 +468,27 @@ class Backend extends Controller
 
     /**
      * Selectpage的实现方法
-     *
-     * 当前方法只是一个比较通用的搜索匹配,请按需重载此方法来编写自己的搜索逻辑,$where按自己的需求写即可
-     * 这里示例了所有的参数，所以比较复杂，实现上自己实现只需简单的几行即可
-     *
      */
     protected function selectpage()
     {
         //设置过滤方法
         $this->request->filter(['trim', 'strip_tags', 'htmlspecialchars']);
 
-        //搜索关键词,客户端输入以空格分开,这里接收为数组
-        $word = (array)$this->request->request("q_word/a");
-        //当前页
-        $page = $this->request->request("pageNumber");
-        //分页大小
-        $pagesize = $this->request->request("pageSize");
-        //搜索条件
-        $andor = $this->request->request("andOr", "and", "strtoupper");
-        //排序方式
-        $orderby = (array)$this->request->request("orderBy/a");
-        //显示的字段
-        $field = $this->request->request("showField");
-        //主键
-        $primarykey = $this->request->request("keyField");
-        //主键值
-        $primaryvalue = $this->request->request("keyValue");
-        //搜索字段
-        $searchfield = (array)$this->request->request("searchField/a");
-        //自定义搜索条件
-        $custom = (array)$this->request->request("custom/a");
-        //是否返回树形结构
-        $istree = $this->request->request("isTree", 0);
-        $ishtml = $this->request->request("isHtml", 0);
-        if ($istree) {
-            $word = [];
-            $pagesize = 999999;
-        }
-        $order = [];
-        foreach ($orderby as $k => $v) {
-            $order[$v[0]] = $v[1];
-        }
-        $field = $field ? $field : 'name';
+        $selectPage = new SelectPage($this->model, $this->selectpageFields);
 
-        //如果有primaryvalue,说明当前是初始化传值
-        if ($primaryvalue !== null) {
-            $where = [$primarykey => ['in', $primaryvalue]];
-            $pagesize = 999999;
-        } else {
-            $where = function ($query) use ($word, $andor, $field, $searchfield, $custom) {
-                $logic = $andor == 'AND' ? '&' : '|';
-                $searchfield = is_array($searchfield) ? implode($logic, $searchfield) : $searchfield;
-                $searchfield = str_replace(',', $logic, $searchfield);
-                $word = array_filter(array_unique($word));
-                if (count($word) == 1) {
-                    $query->where($searchfield, "like", "%" . reset($word) . "%");
-                } else {
-                    $query->where(function ($query) use ($word, $searchfield) {
-                        foreach ($word as $index => $item) {
-                            $query->whereOr(function ($query) use ($item, $searchfield) {
-                                $query->where($searchfield, "like", "%{$item}%");
-                            });
-                        }
-                    });
-                }
-                if ($custom && is_array($custom)) {
-                    foreach ($custom as $k => $v) {
-                        if (is_array($v) && 2 == count($v)) {
-                            $query->where($k, trim($v[0]), $v[1]);
-                        } else {
-                            $query->where($k, '=', $v);
-                        }
-                    }
-                }
-            };
-        }
+        // 数据限制
         $adminIds = $this->getDataLimitAdminIds();
         if (is_array($adminIds)) {
-            $this->model->where($this->dataLimitField, 'in', $adminIds);
+            $selectPage->setDataLimit($this->dataLimit, $this->dataLimitField, $adminIds);
         }
-        $list = [];
-        $total = $this->model->where($where)->count();
-        if ($total > 0) {
-            if (is_array($adminIds)) {
-                $this->model->where($this->dataLimitField, 'in', $adminIds);
-            }
 
-            $fields = is_array($this->selectpageFields) ? $this->selectpageFields : ($this->selectpageFields && $this->selectpageFields != '*' ? explode(',', $this->selectpageFields) : []);
-
-            //如果有primaryvalue,说明当前是初始化传值,按照选择顺序排序
-            if ($primaryvalue !== null && preg_match("/^[a-z0-9_\-]+$/i", $primarykey)) {
-                $primaryvalue = array_unique(is_array($primaryvalue) ? $primaryvalue : explode(',', $primaryvalue));
-                //修复自定义data-primary-key为字符串内容时，给排序字段添加上引号
-                $primaryvalue = array_map(function ($value) {
-                    return \think\Db::quote($value);
-                }, $primaryvalue);
-
-                $primaryvalue = implode(',', $primaryvalue);
-
-                $this->model->orderRaw("FIELD(`{$primarykey}`, {$primaryvalue})");
-            } else {
-                $this->model->order($order);
-            }
-
-            $datalist = $this->model->where($where)
-                ->page($page, $pagesize)
-                ->select();
-
-            foreach ($datalist as $index => $item) {
-                unset($item['password'], $item['salt']);
-                if ($this->selectpageFields == '*') {
-                    $result = [
-                        $primarykey => $item[$primarykey] ?? '',
-                        $field      => $item[$field] ?? '',
-                    ];
-                } else {
-                    $result = array_intersect_key(($item instanceof Model ? $item->toArray() : (array)$item), array_flip($fields));
-                }
-                $result['pid'] = isset($item['pid']) ? $item['pid'] : (isset($item['parent_id']) ? $item['parent_id'] : 0);
-                // 修改为安全的htmlentities调用，兼容php8+版本
-                $result = array_map(function ($value) {
-                    return $value === null ? '' : htmlentities((string)$value);
-                }, $result);
-                $list[] = $result;
-            }
-            if ($istree && !$primaryvalue) {
-                $tree = Tree::instance();
-                $tree->init(collection($list)->toArray(), 'pid');
-                $list = $tree->getTreeList($tree->getTreeArray(0), $field);
-                if (!$ishtml) {
-                    foreach ($list as &$item) {
-                        $item = str_replace('&nbsp;', ' ', $item);
-                    }
-                    unset($item);
-                }
-            }
+        try {
+            $result = $selectPage->execute($this->request->request());
+        } catch (\think\Exception $e) {
+            $this->error($e->getMessage());
         }
-        //这里一定要返回有list这个字段,total是可选的,如果total<=list的数量,则会隐藏分页按钮
-        return json(['list' => $list, 'total' => $total]);
+
+        return json($result);
     }
 
     /**
